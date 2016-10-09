897633dff5664929de2e77cba09c476e94bf6b41
Spencer <sarussell2012@yahoo.com>
2016-10-09 15:14:44 -0600

    I corrected the function so that it plots correctly in the y-axis

--- selfie.r
+++ selfie.r
@@ -2,20 +2,21 @@ rm(list = ls())
 
 
 # Thu Oct 06 20:14:59 2016 ------------------------------
-circ <- function(r, x, xcenter = 0, ycenter = 0){
+circ <- function(r, x, xcenter = 0, ycenter = 0, half = "top"){
 	if (any((abs(x) - abs(xcenter)) > r)) print("There is an error with the x - values that you are entering. The circle extends from the center point at (xcenter, ycenter) out to r. If we choose an x value that is greater than our xcenter plus or minus our radius then we are choosing a point that does not exist in the circle.")
-	y <- ycenter + (r^2 - (x - xcenter)^2)^0.5
+	if (half == "top") return(ycenter + (r^2 - (x - xcenter)^2)^0.5)
+	if (half == "bottom") return(ycenter - (r^2 - (x - xcenter)^2)^0.5)
 }
 
 # The base circle, radius = pi, center at (5, 0)
 plot(-1000, -1000, xlim = c(0, 2*pi), ylim = c(-pi, pi), yaxt = "n", xaxt = "n", ylab = "", xlab = "")
 sp <- c(0, pi - pi * sqrt(2)/2, pi, pi + pi*sqrt(2)/2, pi + pi)
-for (j in c(1,-1)){
-	for (i in 1:4){
-		xcoords <- c(seq(sp[i], sp[i + 1], 0.01), sp[i + 1])
-		y <- j * circ(r = pi, x = xcoords, xcenter = pi)
-		lines(xcoords, y, col = c("red", "blue", "green4", "tomato3")[i])
-	}
+for (i in 1:4){
+	xcoords <- c(seq(sp[i], sp[i + 1], 0.01), sp[i + 1])
+	ytop <- circ(r = pi, x = xcoords, xcenter = pi)
+	ybot <- circ(r = pi, x = xcoords, xcenter = pi, half = "bottom")
+	lines(xcoords, ytop, col = c("red", "blue", "green4", "tomato3")[i])
+	lines(xcoords, ybot, col = c("red", "blue", "green4", "tomato3")[i])
 }
 
 # The eyes
